4d62ee88c832ce6b4469b44defa78c00c4a70ac1
Ehsanul Hoque <ehsanul@ehsanul.com>
2024-12-22 16:00:31 -0800

    Delete topics & messages (#4)
    * remove topics & messages
    
    * delete topics & messages
    
    * cleanup llm comments

--- db/redis_database_test.go
+++ db/redis_database_test.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"sidekick/common"
 	"sidekick/models"
-	"sidekick/utils"
 	"testing"
 	"time"
 
@@ -33,123 +32,9 @@ func newTestRedisDatabase() *RedisDatabase {
 	return db
 }
 
-func TestPersistMessage(t *testing.T) {
-	db := newTestRedisDatabase()
-	workspaceId := "TEST_WORKSPACE_ID"
-	messageRecord := models.Message{
-		WorkspaceId: workspaceId,
-		TopicId:     "topic_" + ksuid.New().String(),
-		Id:          "message_" + ksuid.New().String(),
-		Role:        "testRole",
-		Content:     "testContent",
-		FlowId:      "testWorkflowId",
-		Created:     time.Now().UTC(),
-	}
+// TestPersistMessage has been removed as it is no longer needed
 
-	err := db.PersistMessage(context.Background(), messageRecord)
-	assert.Nil(t, err)
-
-	// Check that the topic sorted set also has the message id
-	sortedSetKey := fmt.Sprintf("%s:%s:messages_sorted_set", messageRecord.WorkspaceId, messageRecord.TopicId)
-	rank, err := db.Client.ZRank(context.Background(), sortedSetKey, messageRecord.Id).Result()
-	assert.Nil(t, err)
-	assert.NotNil(t, rank)
-
-	// Check that the redis string has the desired content
-	key := fmt.Sprintf("%s:%s:%s", messageRecord.WorkspaceId, messageRecord.TopicId, messageRecord.Id)
-	jsonData, err := db.Client.Get(context.Background(), key).Result()
-	assert.Nil(t, err)
-
-	var retrievedMessage models.Message
-	err = json.Unmarshal([]byte(jsonData), &retrievedMessage)
-	assert.Nil(t, err)
-	assert.Equal(t, messageRecord, retrievedMessage)
-}
-
-func TestGetMessages(t *testing.T) {
-	db := newTestRedisDatabase()
-
-	workspaceId := "TEST_WORKSPACE_ID"
-	topicId := "topic_" + ksuid.New().String()
-	messageRecords := []models.Message{
-		{
-			WorkspaceId: workspaceId,
-			TopicId:     topicId,
-			Id:          "message_" + ksuid.New().String(),
-			Role:        "testRole1",
-			Content:     "testContent1",
-			FlowId:      "testWorkflowId1",
-			Created:     time.Now().UTC(),
-		},
-		{
-			WorkspaceId: workspaceId,
-			TopicId:     topicId,
-			Id:          "message_" + ksuid.New().String(),
-			Role:        "testRole2",
-			Content:     "testContent2",
-			FlowId:      "testWorkflowId2",
-			Created:     time.Now().UTC().Add(time.Millisecond),
-		},
-	}
-
-	for _, messageRecord := range messageRecords {
-		err := db.PersistMessage(context.Background(), messageRecord)
-		assert.Nil(t, err)
-	}
-
-	retrievedMessages, err := db.GetMessages(context.Background(), workspaceId, topicId)
-	assert.Nil(t, err)
-	assert.Equal(t, messageRecords, retrievedMessages)
-}
-
-func TestPersistTopic(t *testing.T) {
-	db := newTestRedisDatabase()
-
-	workspaceId := "TEST_WORKSPACE_ID"
-	topicRecord := models.Topic{
-		WorkspaceId: workspaceId,
-		Id:          "topic_" + ksuid.New().String(),
-		Title:       "testTitle",
-		Created:     time.Now().UTC(),
-	}
-
-	err := db.PersistTopic(context.Background(), topicRecord)
-	assert.Nil(t, err)
-
-	// Check that the workspace topics sorted set also has the topic id
-	sortedSetKey := fmt.Sprintf("%s:topics_sorted_set", topicRecord.WorkspaceId)
-	rank, err := db.Client.ZRank(context.Background(), sortedSetKey, topicRecord.Id).Result()
-	assert.Nil(t, err)
-	assert.NotNil(t, rank)
-
-	key := fmt.Sprintf("%s:%s", topicRecord.WorkspaceId, topicRecord.Id)
-	topicJson, err := db.Client.Get(context.Background(), key).Result()
-	if err != nil {
-		t.Fatal(err)
-	}
-	var gotTopicRecord models.Topic
-	if err := json.Unmarshal([]byte(topicJson), &gotTopicRecord); err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, utils.PanicJSON(topicRecord.Created), utils.PanicJSON(gotTopicRecord.Created))
-	assert.Equal(t, topicRecord, gotTopicRecord)
-}
-
-func TestTopicExists(t *testing.T) {
-	db := newTestRedisDatabase()
-
-	// Test when topic exists
-	err := db.Client.Set(context.Background(), "workspace1:topic1", "dummy", 0).Err()
-	assert.NoError(t, err)
-	exists, err := db.TopicExists(context.Background(), "workspace1", "topic1")
-	assert.NoError(t, err)
-	assert.True(t, exists)
-
-	// Test when topic does not exist
-	exists, err = db.TopicExists(context.Background(), "workspace1", "topic2")
-	assert.NoError(t, err)
-	assert.False(t, exists)
-}
+// TestGetMessages has been removed as it is no longer needed
 
 func TestGetTasks(t *testing.T) {
 	db := newTestRedisDatabase()
