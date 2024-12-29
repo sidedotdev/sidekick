a21293831421dfae695c4857f0cd38cdd4cba041
Ehsanul Hoque <ehsanul@ehsanul.com>
2024-12-29 02:55:34 -0500

    cleanup + handle duplicate subscriptions

--- srv/jetstream/flow_event.go
+++ srv/jetstream/flow_event.go
@@ -5,10 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"sidekick/domain"
-	"strconv"
-	"strings"
 	"sync"
-	"time"
 
 	"github.com/nats-io/nats.go/jetstream"
 )
@@ -48,89 +45,6 @@ func (s *Streamer) EndFlowEventStream(ctx context.Context, workspaceId, flowId, 
 	return nil
 }
 
-func (s *Streamer) GetFlowEvents(ctx context.Context, workspaceId string, streamKeys map[string]string, maxCount int64, blockDuration time.Duration) ([]domain.FlowEvent, map[string]string, error) {
-	if maxCount == 0 {
-		maxCount = 100
-	}
-
-	newStreamKeys := make(map[string]string)
-	var events []domain.FlowEvent
-
-	// Process each flow ID stream separately
-	for streamKey, startId := range streamKeys {
-		parts := strings.Split(streamKey, ":")
-		if len(parts) != 4 || parts[0] != workspaceId || parts[2] != "stream" {
-			return nil, nil, fmt.Errorf("invalid stream key format: %s", streamKey)
-		}
-		flowEventParentId := parts[3]
-		subject := fmt.Sprintf("flow_events.%s.%s", workspaceId, flowEventParentId)
-
-		// default to starting from the beginning for flow events
-		if startId == "" {
-			startId = "0"
-		} else if startId == "$" {
-			return nil, nil, fmt.Errorf("$ not supported for flow events, but received for flow %s", flowEventParentId)
-		}
-
-		var deliveryPolicy jetstream.DeliverPolicy
-		var startTime *time.Time
-		var startSeq uint64
-
-		if startId == "0" {
-			deliveryPolicy = jetstream.DeliverAllPolicy
-		} else {
-			deliveryPolicy = jetstream.DeliverByStartSequencePolicy
-			var err error
-			startSeq, err = strconv.ParseUint(startId, 10, 64)
-			if err != nil {
-				return nil, nil, fmt.Errorf("invalid stream message start id for flow %s: %w", flowEventParentId, err)
-			}
-		}
-
-		consumer, err := s.js.OrderedConsumer(ctx, PersistentStreamName, jetstream.OrderedConsumerConfig{
-			FilterSubjects:    []string{subject},
-			InactiveThreshold: 5 * time.Minute,
-			DeliverPolicy:     deliveryPolicy,
-			OptStartSeq:       startSeq,
-			OptStartTime:      startTime,
-		})
-		if err != nil && err != jetstream.ErrConsumerNameAlreadyInUse {
-			return nil, nil, fmt.Errorf("failed to create consumer for flow %s: %w", flowEventParentId, err)
-		}
-
-		// Pull messages
-		waitPerKey := blockDuration / time.Duration(len(streamKeys))
-		msgs, err := consumer.Fetch(int(maxCount), jetstream.FetchMaxWait(waitPerKey))
-		if err != nil {
-			return nil, nil, fmt.Errorf("failed to fetch messages for flow %s: %w", flowEventParentId, err)
-		}
-
-		var lastSequence uint64
-		for msg := range msgs.Messages() {
-			event, err := domain.UnmarshalFlowEvent(msg.Data())
-			if err != nil {
-				return nil, nil, fmt.Errorf("failed to unmarshal flow event: %w", err)
-			}
-			events = append(events, event)
-			msg.Ack()
-
-			meta, err := msg.Metadata()
-			if err != nil {
-				return nil, nil, fmt.Errorf("failed to get message metadata: %w", err)
-			}
-			lastSequence = meta.Sequence.Stream
-		}
-
-		if lastSequence > 0 {
-			newStreamKeys[streamKey] = fmt.Sprintf("%d", lastSequence+1)
-		} else {
-			newStreamKeys[streamKey] = startId
-		}
-	}
-
-	return events, newStreamKeys, nil
-}
-
 func (s *Streamer) StreamFlowEvents(ctx context.Context, workspaceId, flowId string, streamMessageStartId string, eventParentIdCh <-chan string) (<-chan domain.FlowEvent, <-chan error) {
 	eventCh := make(chan domain.FlowEvent)
 	errCh := make(chan error, 1)
@@ -140,6 +54,7 @@ func (s *Streamer) StreamFlowEvents(ctx context.Context, workspaceId, flowId str
 		streamMessageStartId = "0"
 	}
 
+	eventParentIdSet := make(map[string]bool)
 	go func() {
 		defer close(eventCh)
 		defer close(errCh)
@@ -154,6 +69,10 @@ func (s *Streamer) StreamFlowEvents(ctx context.Context, workspaceId, flowId str
 				if !ok {
 					break outer
 				}
+				// only create a consumer for each event parent ID once
+				if eventParentIdSet[eventParentId] {
+					continue
+				}
 				subject := fmt.Sprintf("flow_events.%s.%s", workspaceId, eventParentId)
 
 				consumer, err := s.createConsumer(ctx, subject, streamMessageStartId)
@@ -164,6 +83,7 @@ func (s *Streamer) StreamFlowEvents(ctx context.Context, workspaceId, flowId str
 
 				wg.Add(1)
 				go s.consumeFlowEvents(ctx, consumer, eventCh, errCh, wg)
+				eventParentIdSet[eventParentId] = true
 			}
 		}
 		wg.Wait()
