4448659f75c2f6fd0c6b1884cb205865d7a20cbc
Ehsanul Hoque <ehsanul@ehsanul.com>
2024-12-29 23:34:26 -0500

    styling

--- frontend/src/components/SegmentedControl.vue
+++ frontend/src/components/SegmentedControl.vue
@@ -31,27 +31,25 @@ defineEmits<{
 <style scoped>
 .segmented-control {
   display: inline-flex;
-  border: 1px solid var(--color-border);
-  border-radius: 0.25rem;
+  border: 1px solid var(--color-border-contrast);
+  border-radius: 0.5rem;
   overflow: hidden;
 }
 
 button {
+  border: 0;
   padding: 0.5rem 1rem;
   background-color: var(--color-background);
   color: var(--color-text);
-  border: none;
   cursor: pointer;
   transition: background-color 0.3s, color 0.3s;
   font-size: var(--font-size);
 }
 
-button:not(:last-child) {
-  border-right: 1px solid var(--color-border);
-}
-
 button.active {
   background-color: var(--color-background-hover);
   box-shadow: inset 0 0 1px 1px var(--color-border-hover);
+  border-radius: 0.5rem;
+  border-width: 0;
 }
 </style>
