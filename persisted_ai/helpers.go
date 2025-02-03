8732a8c52e33cc6cf7f5ca15603be073b2b38722
Ehsanul Hoque <ehsanul@ehsanul.com>
2025-02-02 21:17:29 -0800

    handle forced tool call in content
    o3-mini has a tendency to use the content instead of tool call when
    forced to use a single specific tool. this workaround handles that
    situation by treating the content as if it was a tool call, in the case
    that it parses as json and we forced a single tool to be used.

--- persisted_ai/helpers.go
+++ persisted_ai/helpers.go
@@ -2,6 +2,7 @@ package persisted_ai
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"sidekick/common"
@@ -9,7 +10,9 @@ import (
 	"sidekick/flow_action"
 	"sidekick/llm"
 	"sidekick/utils"
+	"strings"
 
+	"github.com/google/uuid"
 	"go.temporal.io/sdk/workflow"
 )
 
@@ -72,9 +75,15 @@ func ForceToolCall(actionCtx flow_action.ActionContext, llmConfig common.LLMConf
 		options.FlowActionId = flowAction.Id
 		var chatResponse llm.ChatMessageResponse
 		err := workflow.ExecuteActivity(utils.LlmHeartbeatCtx(actionCtx), la.ChatStream, options).Get(actionCtx, &chatResponse)
-		if err == nil {
-			(*params).Messages = append(params.Messages, chatResponse.ChatMessage)
+
+		if err == nil && len(chatResponse.Content) > 0 && len(tools) == 1 {
+			if toolCall, ok := ParseForcedToolCall(tools[0], chatResponse.Content); ok {
+				chatResponse.Content = ""
+				chatResponse.ToolCalls = []llm.ToolCall{toolCall}
+				return chatResponse, nil
+			}
 		}
+
 		return chatResponse, err
 	})
 
@@ -87,26 +96,64 @@ func ForceToolCall(actionCtx flow_action.ActionContext, llmConfig common.LLMConf
 	// that succeeds. Note that this won't check for required fields and some
 	// other aspects of the schema. for this case, we need to use
 	if err == nil && len(chatResponse.ToolCalls) == 0 {
-		(*params).Messages = append(params.Messages, llm.ChatMessage{
+
+		options.Params.Messages = append(options.Params.Messages, chatResponse.ChatMessage)
+		options.Params.Messages = append(options.Params.Messages, llm.ChatMessage{
 			Role:    llm.ChatMessageRoleSystem,
 			Content: "Expected a tool call, but didn't get it. Embedding the json in the content is not sufficient. Please use the provided tool(s).",
 		})
-		options.Params.Messages = params.Messages
 		actionCtx.ActionParams = options.ActionParams()
 		chatResponse, err = flow_action.Track(actionCtx, func(flowAction domain.FlowAction) (llm.ChatMessageResponse, error) {
-			var chatResponse llm.ChatMessageResponse
+			var chatResponse2 llm.ChatMessageResponse
 			options.FlowActionId = flowAction.Id
-			err := workflow.ExecuteActivity(utils.LlmHeartbeatCtx(actionCtx), la.ChatStream, options).Get(actionCtx, &chatResponse)
+			err := workflow.ExecuteActivity(utils.LlmHeartbeatCtx(actionCtx), la.ChatStream, options).Get(actionCtx, &chatResponse2)
 			if err == nil {
-				(*params).Messages = append(params.Messages, chatResponse.ChatMessage)
+				if len(chatResponse2.Content) > 0 && len(tools) == 1 {
+					if toolCall, ok := ParseForcedToolCall(tools[0], chatResponse2.Content); ok {
+						chatResponse2.Content = ""
+						chatResponse2.ToolCalls = []llm.ToolCall{toolCall}
+						return chatResponse2, nil
+					}
+				}
 
-				if len(chatResponse.ToolCalls) == 0 {
-					return chatResponse, fmt.Errorf("no tool calls found in llm response")
+				if len(chatResponse2.ToolCalls) == 0 {
+					return chatResponse2, fmt.Errorf("no tool calls found in llm response")
 				}
 			}
-			return chatResponse, err
+			return chatResponse2, err
 		})
 	}
 
+	if err == nil {
+		(*params).Messages = append(params.Messages, chatResponse.ChatMessage)
+	}
 	return &chatResponse, err
+}
+
+// o3-mini seems to want to put its tool calls into the content, so we're
+// handling that by parsing it into a tool call, in the case we are forcing a
+// single tool call. if the content is valid json, we infer it to be the tool
+// call arguments
+func ParseForcedToolCall(tool *llm.Tool, s string) (llm.ToolCall, bool) {
+	parsedJson := map[string]interface{}{}
+	err := json.Unmarshal([]byte(s), &parsedJson)
+	if err != nil {
+		return llm.ToolCall{}, false
+	}
+
+	// to avoid max id length errors, generate very short fake unique id string,
+	// not in usual uuid format, via base64 encoding of the UUID ID.
+	// https://go.dev/play/p/hcKURudLpZ
+	idBytes, err := uuid.New().MarshalBinary()
+	if err != nil {
+		panic(err)
+	}
+	var escaper = strings.NewReplacer("9", "99", "-", "90", "_", "91")
+	id := escaper.Replace(base64.RawURLEncoding.EncodeToString(idBytes))
+
+	return llm.ToolCall{
+		Id: "fromcontent_" + id,
+		Name: tool.Name,
+		Arguments: s,
+	}, true
 }
