c7cf66c3170c46d2c6800667070632e1420d89f6
Ehsanul Hoque <ehsanul@ehsanul.com>
2025-07-13 15:39:48 -0700

    bugfix empty workspace config editing in frontend

--- frontend/src/components/WorkspaceForm.vue
+++ frontend/src/components/WorkspaceForm.vue
@@ -60,14 +60,21 @@ const emit = defineEmits<{
 
 const name = ref('');
 const localRepoDir = ref('');
+const getEmptyUseCaseConfigs = () => { return {'': [{ provider: '', model: '' }]}}
 const llmConfig = ref<LLMConfig>({ 
   defaults: props.workspace.llmConfig?.defaults?.length ? [...props.workspace.llmConfig.defaults] : [{ provider: '', model: '' }], 
-  useCaseConfigs: props.workspace.llmConfig?.useCaseConfigs ? { ...props.workspace.llmConfig.useCaseConfigs } : {'': [{ provider: '', model: '' }]}
+  useCaseConfigs: props.workspace.llmConfig?.useCaseConfigs ? { ...props.workspace.llmConfig.useCaseConfigs } : getEmptyUseCaseConfigs() 
 });
+if (!Object.keys(llmConfig.value.useCaseConfigs).length) {
+  llmConfig.value.useCaseConfigs = getEmptyUseCaseConfigs() 
+}
 const embeddingConfig = ref<EmbeddingConfig>({ 
   defaults: props.workspace.embeddingConfig?.defaults?.length ? [...props.workspace.embeddingConfig.defaults] : [{ provider: '', model: '' }], 
-  useCaseConfigs: props.workspace.embeddingConfig?.useCaseConfigs ? { ...props.workspace.embeddingConfig.useCaseConfigs } : {'': [{ provider: '', model: '' }]} 
+  useCaseConfigs: props.workspace.embeddingConfig?.useCaseConfigs ? { ...props.workspace.embeddingConfig.useCaseConfigs } : getEmptyUseCaseConfigs() 
 });
+if (!Object.keys(embeddingConfig.value.useCaseConfigs).length) {
+  embeddingConfig.value.useCaseConfigs = getEmptyUseCaseConfigs() 
+}
 
 const llmAdvancedExpanded = ref(false);
 
@@ -120,8 +127,8 @@ const submitWorkspace = async () => {
   const formData: Omit<Workspace, 'id'> = {
     name: name.value,
     localRepoDir: localRepoDir.value,
-    llmConfig: filterEmptyUseCaseKeys(llmConfig.value),
-    embeddingConfig: filterEmptyUseCaseKeys(embeddingConfig.value)
+    llmConfig: llmConfig.value,
+    embeddingConfig: embeddingConfig.value
   };
 
   try {
