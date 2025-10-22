3adc22dda2ad10aa0bd5186253cbb0d341288169
Sidekick <sidekick@side.dev>
2025-10-22 15:26:34 -0700

    Add ContextTypeInitialInstructions to all instruction chat messages that serve as initial instructio...
    ...ns for LLM workflows. This ensures proper chat history management by the ManageChatHistoryV2Activity, which uses ContextType markers to determine which messages to retain during history trimming.

--- dev/code_context.go
+++ dev/code_context.go
@@ -442,6 +442,7 @@ func addCodeContextPrompt(chatHistory *[]llm.ChatMessage, promptInfo PromptInfo)
 	skip := false
 	isError := false
 	cacheControl := ""
+	contextType := ""
 	switch info := promptInfo.(type) {
 	case SkipInfo:
 		skip = true
@@ -456,9 +457,11 @@ func addCodeContextPrompt(chatHistory *[]llm.ChatMessage, promptInfo PromptInfo)
 	case DetermineCodeContextInfo:
 		content = renderCodeContextInitialPrompt(info)
 		cacheControl = "ephemeral"
+		contextType = ContextTypeInitialInstructions
 	case RefineCodeContextInfo:
 		content = renderCodeContextRefineAndRankPrompt(info)
 		cacheControl = "ephemeral"
+		contextType = ContextTypeInitialInstructions
 	default:
 		panic("Unsupported prompt type for code context: " + promptInfo.GetType())
 	}
@@ -470,6 +473,7 @@ func addCodeContextPrompt(chatHistory *[]llm.ChatMessage, promptInfo PromptInfo)
 			Name:         name,
 			ToolCallId:   toolCallId,
 			CacheControl: cacheControl,
+			ContextType:  contextType,
 			IsError:      isError,
 		})
 	}
