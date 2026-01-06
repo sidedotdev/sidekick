1733598c19234f77853bdea84f8106c01227602e
Ehsanul Hoque <ehsanul@ehsanul.com>
2026-01-06 14:56:11 -0800

    'create a flag for the run_activity script that, instead of running the activity via a new workflow, ...\n\n...it'\''ll directly run the activity directly (i.e. execute the function with the right arguments, rather than using .'

--- scripts/run_activity/main.go
+++ scripts/run_activity/main.go
@@ -6,9 +6,25 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"reflect"
+	"runtime"
+	"sidekick"
+	"strings"
 	"time"
 
+	"sidekick/coding"
+	"sidekick/coding/git"
+	"sidekick/coding/lsp"
+	"sidekick/coding/tree_sitter"
 	"sidekick/common"
+	"sidekick/dev"
+	"sidekick/env"
+	"sidekick/fflag"
+	"sidekick/flow_action"
+	"sidekick/persisted_ai"
+	"sidekick/poll_failures"
+	"sidekick/srv"
+	"sidekick/workspace"
 
 	"github.com/google/uuid"
 	"github.com/rs/zerolog"
@@ -45,16 +61,215 @@ func argsToInterfaces(args []json.RawMessage) []interface{} {
 	return result
 }
 
+func getFunctionName(fn interface{}) string {
+	fullName := runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name()
+	parts := strings.Split(fullName, ".")
+	name := parts[len(parts)-1]
+	// Remove -fm suffix that Go adds for method values
+	name = strings.TrimSuffix(name, "-fm")
+	return name
+}
+
+func buildActivityRegistry() map[string]interface{} {
+	registry := make(map[string]interface{})
+
+	service, err := sidekick.GetService()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to initialize storage for direct execution")
+	}
+
+	featureFlag, err := fflag.NewFFlag("flags.yml")
+	if err != nil {
+		log.Warn().Err(err).Msg("Failed to create go-feature-flag instance, some activities may not work")
+	}
+	ffa := fflag.FFlagActivities{FFlag: featureFlag}
+
+	hostPort := common.GetTemporalServerHostPort()
+	tracingInterceptor, _ := opentelemetry.NewTracingInterceptor(opentelemetry.TracerOptions{})
+	temporalClient, err := client.Dial(client.Options{
+		HostPort:     hostPort,
+		Interceptors: []interceptor.ClientInterceptor{tracingInterceptor},
+	})
+	if err != nil {
+		log.Warn().Err(err).Msg("Failed to create Temporal client, some activities may not work")
+	}
+
+	devManagerActivities := &dev.DevAgentManagerActivities{
+		Storage:        service,
+		TemporalClient: temporalClient,
+	}
+	flowActivities := &flow_action.FlowActivities{Service: service}
+	embedActivities := &persisted_ai.EmbedActivities{
+		Storage: service,
+	}
+	llmActivities := &persisted_ai.LlmActivities{
+		Streamer: service,
+	}
+	lspActivities := &lsp.LSPActivities{
+		LSPClientProvider: func(languageName string) lsp.LSPClient {
+			return &lsp.Jsonrpc2LSPClient{
+				LanguageName: languageName,
+			}
+		},
+		InitializedClients: map[string]lsp.LSPClient{},
+	}
+	treeSitterActivities := &tree_sitter.TreeSitterActivities{
+		DatabaseAccessor: service,
+	}
+	codingActivities := &coding.CodingActivities{
+		TreeSitterActivities: treeSitterActivities,
+		LSPActivities:        lspActivities,
+	}
+	vectorActivities := &persisted_ai.VectorActivities{
+		DatabaseAccessor: service,
+	}
+	ragActivities := &persisted_ai.RagActivities{
+		DatabaseAccessor: service,
+	}
+	pollFailuresActivities := &poll_failures.PollFailuresActivities{
+		TemporalClient: temporalClient,
+		Service:        service,
+	}
+	devActivities := &dev.DevActivities{
+		LSPActivities: lspActivities,
+	}
+	devRunActivities := &dev.DevRunActivities{
+		Streamer: service,
+	}
+	srvActivities := &srv.Activities{Service: service}
+	workspaceActivities := &workspace.Activities{Storage: service}
+
+	// Register standalone functions
+	standaloneFuncs := []interface{}{
+		env.NewLocalGitWorktreeActivity,
+		sidekick.GithubCloneRepoActivity,
+		env.EnvRunCommandActivity,
+		git.GitDiffActivity,
+		git.GitAddActivity,
+		git.GitRestoreActivity,
+		git.GitCommitActivity,
+		git.GitCheckoutActivity,
+		git.GitMergeActivity,
+		git.ListWorktreesActivity,
+		git.CleanupWorktreeActivity,
+		git.GetCurrentBranch,
+		git.GetDefaultBranch,
+		git.ListLocalBranches,
+		git.WriteTreeActivity,
+		dev.GetRepoConfigActivity,
+		dev.GetRepoConfigActivityV2,
+		dev.GetSymbolsActivity,
+		dev.ApplyEditBlocksActivity,
+		dev.ReadFileActivity,
+		dev.BulkReadFileActivity,
+		dev.ManageChatHistoryActivity,
+		dev.ManageChatHistoryV2Activity,
+		common.GetLocalConfig,
+		common.BaseCommandPermissionsActivity,
+	}
+	for _, fn := range standaloneFuncs {
+		registry[getFunctionName(fn)] = fn
+	}
+
+	// Register struct methods
+	registerStructMethods(registry, srvActivities)
+	registerStructMethods(registry, llmActivities)
+	registerStructMethods(registry, pollFailuresActivities)
+	registerStructMethods(registry, lspActivities)
+	registerStructMethods(registry, treeSitterActivities)
+	registerStructMethods(registry, codingActivities)
+	registerStructMethods(registry, ragActivities)
+	registerStructMethods(registry, embedActivities)
+	registerStructMethods(registry, vectorActivities)
+	registerStructMethods(registry, flowActivities)
+	registerStructMethods(registry, devManagerActivities)
+	registerStructMethods(registry, devActivities)
+	registerStructMethods(registry, devRunActivities)
+	registerStructMethods(registry, workspaceActivities)
+	registry["EvalBoolFlag"] = ffa.EvalBoolFlag
+
+	return registry
+}
+
+func registerStructMethods(registry map[string]interface{}, structPtr interface{}) {
+	val := reflect.ValueOf(structPtr)
+	typ := val.Type()
+	for i := 0; i < typ.NumMethod(); i++ {
+		method := typ.Method(i)
+		if method.IsExported() {
+			registry[method.Name] = val.Method(i).Interface()
+		}
+	}
+}
+
+func executeActivityDirect(activityName string, activityArgs []json.RawMessage, timeout time.Duration) (json.RawMessage, error) {
+	registry := buildActivityRegistry()
+
+	activityFn, ok := registry[activityName]
+	if !ok {
+		return nil, fmt.Errorf("activity %q not found in registry", activityName)
+	}
+
+	fnVal := reflect.ValueOf(activityFn)
+	fnType := fnVal.Type()
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	// Build arguments: first arg is context, rest are from JSON
+	args := make([]reflect.Value, fnType.NumIn())
+	args[0] = reflect.ValueOf(ctx)
+
+	for i := 1; i < fnType.NumIn(); i++ {
+		argType := fnType.In(i)
+		argPtr := reflect.New(argType)
+		if i-1 < len(activityArgs) {
+			if err := json.Unmarshal(activityArgs[i-1], argPtr.Interface()); err != nil {
+				return nil, fmt.Errorf("failed to unmarshal argument %d: %w", i-1, err)
+			}
+		}
+		args[i] = argPtr.Elem()
+	}
+
+	results := fnVal.Call(args)
+
+	// Handle return values: (result, error) or just (error)
+	var resultVal reflect.Value
+	var errVal reflect.Value
+	if len(results) == 2 {
+		resultVal = results[0]
+		errVal = results[1]
+	} else if len(results) == 1 {
+		errVal = results[0]
+	}
+
+	if !errVal.IsNil() {
+		return nil, errVal.Interface().(error)
+	}
+
+	if resultVal.IsValid() {
+		resultJSON, err := json.Marshal(resultVal.Interface())
+		if err != nil {
+			return nil, fmt.Errorf("failed to marshal result: %w", err)
+		}
+		return resultJSON, nil
+	}
+
+	return json.RawMessage("null"), nil
+}
+
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
 	var timeout time.Duration
+	var direct bool
 	flag.DurationVar(&timeout, "timeout", 180*time.Second, "Timeout for the activity execution")
+	flag.BoolVar(&direct, "direct", false, "Execute activity directly without Temporal workflow")
 	flag.Parse()
 
 	args := flag.Args()
 	if len(args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %s [--timeout duration] <activity_name> <json_file_path>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s [--timeout duration] [--direct] <activity_name> <json_file_path>\n", os.Args[0])
 		os.Exit(1)
 	}
 
@@ -73,19 +288,48 @@ func main() {
 		os.Exit(1)
 	}
 
+	var result json.RawMessage
+
+	if direct {
+		log.Info().Str("activity", activityName).Msg("Executing activity directly")
+		result, err = executeActivityDirect(activityName, activityArgs, timeout)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error executing activity: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		result, err = executeActivityViaWorkflow(activityName, activityArgs, timeout)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error executing activity via workflow: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	var prettyResult interface{}
+	if err := json.Unmarshal(result, &prettyResult); err != nil {
+		fmt.Println(string(result))
+	} else {
+		prettyJSON, err := json.MarshalIndent(prettyResult, "", "  ")
+		if err != nil {
+			fmt.Println(string(result))
+		} else {
+			fmt.Println(string(prettyJSON))
+		}
+	}
+}
+
+func executeActivityViaWorkflow(activityName string, activityArgs []json.RawMessage, timeout time.Duration) (json.RawMessage, error) {
 	hostPort := common.GetTemporalServerHostPort()
 	tracingInterceptor, err := opentelemetry.NewTracingInterceptor(opentelemetry.TracerOptions{})
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error creating tracing interceptor: %v\n", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("error creating tracing interceptor: %w", err)
 	}
 	temporalClient, err := client.Dial(client.Options{
 		HostPort:     hostPort,
 		Interceptors: []interceptor.ClientInterceptor{tracingInterceptor},
 	})
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error connecting to Temporal: %v\n", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("error connecting to Temporal: %w", err)
 	}
 	defer temporalClient.Close()
 
@@ -93,8 +337,7 @@ func main() {
 	w.RegisterWorkflow(RunActivityWorkflow)
 	err = w.Start()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error starting worker: %v\n", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("error starting worker: %w", err)
 	}
 	defer w.Stop()
 
@@ -109,26 +352,14 @@ func main() {
 		TaskQueue: runActivityTaskQueue,
 	}, RunActivityWorkflow, activityName, activityArgs)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error executing workflow: %v\n", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("error executing workflow: %w", err)
 	}
 
 	var result json.RawMessage
 	err = workflowRun.Get(ctx, &result)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error getting workflow result: %v\n", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("error getting workflow result: %w", err)
 	}
 
-	var prettyResult interface{}
-	if err := json.Unmarshal(result, &prettyResult); err != nil {
-		fmt.Println(string(result))
-	} else {
-		prettyJSON, err := json.MarshalIndent(prettyResult, "", "  ")
-		if err != nil {
-			fmt.Println(string(result))
-		} else {
-			fmt.Println(string(prettyJSON))
-		}
-	}
+	return result, nil
 }
