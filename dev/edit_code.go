221bf4e5282b561607c3444e9e1b62653cd6ddcf
Ehsanul Hoque <ehsanul@ehsanul.com>
2026-03-02 02:43:42 -0800

    lost the read image tool

--- dev/edit_code.go
+++ dev/edit_code.go
@@ -539,6 +539,10 @@ func buildAuthorEditBlockInput(dCtx DevContext, codingModelConfig common.ModelCo
 	tools = append(tools, &bulkReadFileTool)
 	tools = append(tools, &runCommandTool)
 
+	if supportsImageToolResults(codingModelConfig) {
+		tools = append(tools, &readImageTool)
+	}
+
 	if dCtx.Worktree != nil {
 		tools = append(tools, &setBaseBranchTool)
 	}
